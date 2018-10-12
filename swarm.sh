12e38ed7492e39b614f72a20efa6c64c95add1cc
Reza  Purnama Arief <reza.arief@bonial.com>
2018-10-12 17:23:31 +0200

    first commit

---
+++ swarm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for i in berlin paris rome prague; do
+	docker-machine create -d virtualbox swarm-$i
+done
+echo "MACHINE ARE CREATED."
+eval $(docker-machine env swarm-berlin)
+docker swarm init --advertise-addr $(docker-machine ip swarm-berlin)
+docker run -it -d -p 5000:8080 -v /var/run/docker.sock:/var/run/docker.sock dockersamples/visualizer
+TOKEN=$(docker swarm join-token -q manager)
+INIT_IP=$(docker-machine ip swarm-berlin)
+echo $(docker-machine ip swarm-berlin)" IS MANAGER"
+for i in berlin paris rome prague; do
+	eval $(docker-machine env swarm-$i)
+	docker swarm join --advertise-addr $(docker-machine ip swarm-$i) --token $TOKEN $INIT_IP:2377
+done
